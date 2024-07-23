37b41bce2a602c0ad5bf1a2cfde80f1f2627c568
varucard <arielmolus25@gmail.com>
2024-07-22 23:46:23 -0300

    Conexión a internet Conexión a MySql Pantalla Leds

--- Arduino/arduino/pfc/pfc.ino
+++ Arduino/arduino/pfc/pfc.ino
@@ -1,18 +1,19 @@
 #include <Ethernet.h>
 #include <MySQL_Connection.h>
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 
-// Configuración de la pantalla LCD
-const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+// Configuración de la pantalla LCD I2C
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 // Configuración de los LEDs
-const int ledVerde = 4;
-const int ledRojo = 5;
+const int ledVerde = 4;    // LED Indicador de que esta conectado a Internet y MySql
+const int ledRojo = 5;     // LED algo fallo
+const int ledAmarillo = 6; // LED el Equipo esta andando
+const int ledAzul = 7;     // LED el Equipo esta leyendo
 
 // Datos de configuración de Internet y MySQL
 byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress server_addr(192, 168, 1, 37); // IP del MySQL *server*
+IPAddress server_addr(192, 168, 1, 40); // IP del MySQL *server*
 IPAddress ip(192, 168, 1, 36);          // IP de Arduino
 IPAddress subnet(255, 255, 255, 0);     // Sub-Mascara Arduino
 IPAddress gateway(192, 168, 1, 1);      // Puerta de acceso Arduino
@@ -24,63 +25,88 @@ EthernetClient client;
 MySQL_Connection conn((Client *)&client);
 
 void setup() {
-  Serial.begin(115200);
-  lcd.begin(20, 4); // Iniciar la pantalla LCD con 20 columnas y 4 filas
+  // Iniciar la pantalla LCD
+  lcd.init(); 
+  lcd.backlight();
 
+  // Configurar LEDs
   pinMode(ledVerde, OUTPUT);
   pinMode(ledRojo, OUTPUT);
+  pinMode(ledAmarillo, OUTPUT);
+  pinMode(ledAzul, OUTPUT);
   digitalWrite(ledVerde, LOW);
   digitalWrite(ledRojo, LOW);
-
-  while (!Serial); // Esperar a la conexion serial
+  digitalWrite(ledAmarillo, HIGH);
+  digitalWrite(ledAzul, LOW);
 
   // Mostrar mensaje inicial
   lcd.setCursor(0, 0);
-  Serial.println("Iniciando conexion");
-  lcd.print("Iniciando conexion");
+  lcd.print("Palillo");
+  lcd.setCursor(8, 1);
+  lcd.print("Fight");
+  lcd.setCursor(14, 2);
+  lcd.print("Club!");
+  lcd.setCursor(0, 3);
+  lcd.print("Bienvenido Admin!");
+  delay(5000);
 
   // Iniciar la conexión con la red
-  if (Ethernet.begin(mac_addr) == 0) {
-    Serial.println("Error al configurar Ethernet usando DHCP");
-    lcd.setCursor(0, 1);
-    lcd.print("Error Ethernet DHCP");
-    digitalWrite(ledRojo, HIGH); // Encender LED rojo
-    // No es posible continuar, así que no hacemos nada más
-    for (;;);
-  }
-  
-  // Mostrar IP en la LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Intentando");
   lcd.setCursor(0, 1);
-  lcd.print("IP: ");
+  lcd.print("Conectar a la Red");
+  delay(5000);
+
+  Ethernet.begin(mac_addr, ip, gateway, gateway, subnet); // Configurar Ethernet manualmente
+  delay(2000);
+
+  // Mostrar datos conexión a la red
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Conectado a la red");
+  lcd.setCursor(0, 1);
+  lcd.print("IP Arduino: ");
+  lcd.setCursor(0, 2);
   lcd.print(Ethernet.localIP());
-  Serial.print("IP del Arduino: ");
-  Serial.println(Ethernet.localIP());
+  delay(5000);
   
   // Intentar conectar a MySQL
-  lcd.setCursor(0, 2);
+  lcd.clear();
+  lcd.setCursor(0, 0);
   lcd.print("Conectando a MySQL");
-  Serial.println("Conectando a MySQL...");
+  lcd.setCursor(0, 1);
+  lcd.print("Aguarde por favor");
+  delay(5000);
+  
   if (conn.connect(server_addr, port, user, password)) {
-    Serial.println("Conexión con la base de datos exitosa.");
     lcd.setCursor(0, 3);
     lcd.print("Conexion a MySQL OK");
-    digitalWrite(ledVerde, HIGH); // Encender LED verde
-    delay(1000);
-    // Aquí puedes agregar tu código para ejecutar consultas a la base de datos.
+    digitalWrite(ledVerde, HIGH);
+    delay(5000);
   } else {
-    Serial.println("Conexión con la base de datos fallida.");
-    lcd.setCursor(0, 3);
-    lcd.print("Conexion MySQL Fallida");
-    digitalWrite(ledRojo, HIGH); // Encender LED rojo
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Error conexion MySQL");
+    lcd.setCursor(0, 2);
+    lcd.print("Por favor, reinicie");
+    digitalWrite(ledRojo, HIGH);
+    delay(60000);
   }
   
   conn.close();
-  delay(2000);
+  delay(5000);
   
-  // Mostrar mensaje final en la LCD
+  // Mostrar mensaje sistema
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Palillo Fight Club");
+  lcd.print("Palillo");
+  lcd.setCursor(8, 1);
+  lcd.print("Fight");
+  lcd.setCursor(14, 2);
+  lcd.print("Club!");
+  lcd.setCursor(0, 3);
+  lcd.print("Bienvenidos!");
 }
 
 void loop() {
